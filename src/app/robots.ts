1d238aa9181f5a46b7a57676ad1bec09ea81088a
Tunjilor <shoptunji@gmail.com>
2026-05-11 05:32:36 +0000

    fix: SEO - add API disallow and host to robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,9 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
+      disallow: "/api/",
     },
     sitemap: "https://www.freecalcs.io/sitemap.xml",
+    host: "https://www.freecalcs.io",
   };
 }
